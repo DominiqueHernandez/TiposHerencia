8df7a3c8d5a4ad84f53e4ba3c475514994dbd2d5
DominiqueHernandez <151699752+DominiqueHernandez@users.noreply.github.com>
2023-11-21 21:04:46 -0600

    Add files via upload

---
+++ Herencia_Construccion/Avion.java
@@ -0,0 +1,25 @@
+	    
+	    
+	    public void Descender(){
+	        System.out.println("Esta decendiendo");
+	    }
+	     public void setAlas(int alas){
+	         this.alas=alas;
+	    
+	     }
+	    public int getAlas(){
+	        return alas;
+	    }
+	    public int getRuedas(){
+	        return ruedas;
+	    }
+	    public int getAltura(){
+	        return altura;
+	    }
+	    
+	    public boolean getCola(){
+	        return cola;
+	        
+	    }
+	    
+}
---
+++ Herencia_Construccion/Cohete.java
@@ -0,0 +1,23 @@
+
+public class Cohete extends Avion  {
+
+	private String color;
+	private int numDePropulsores;
+	private long velocidad;
+	public void Despegar() {
+		
+	}
+	public void lanzar() {
+		
+	}
+	public long getVelocidad() {
+		return velocidad;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+
+	public int getNumDePropulsores() {
+		return numDePropulsores;
+	}
